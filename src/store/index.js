a92ef9b1b9cdda7b3767745aab52876e3b2ff8d7
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-11-09 14:55:07 +0900

    ADD: vuexでオブジェクトを表示

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    message: 'Hello Vuexほに',
+    users: [
+      { name: 'John', email: 'john@example.com', age: 22 },
+      { name: 'Merry', email: 'merry@facebook.com', age: 33 },
+      { name: 'Ken', email: 'ken@amazon.com', age: 29 },
+    ],
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
